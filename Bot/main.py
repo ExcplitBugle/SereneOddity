7a429d65eceaf98b450367ef36e79e823fcbda3a
Braiden <61211556+ExcplitBugle@users.noreply.github.com>
2021-03-22 03:35:27 -0400

    Update main.py
    Added Base!

--- Bot/main.py
+++ Bot/main.py
@@ -1,1 +1,24 @@
-# Junk so i can upload it to github
+# == Discord Imports == #
+import discord
+from discord.ext import commands
+
+# == Python Moduel Imports == #
+import os
+from pathlib import Path
+
+# == Var Imports == #
+from constants import Tokens
+
+
+class Bot(commands.Bot):
+
+    def __init__(self) -> None:
+        intents = discord.Intents.all()
+
+        super().__init__(command_prefix="?", intents=intents)
+
+    async def on_ready(self):
+        print("Logged in!")
+
+bot = Bot()
+bot.run(Tokens.BOT_TOKEN)
