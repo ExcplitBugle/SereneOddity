6b6f0a94e33a5425448417c6d0e16441c31bdb07
Braiden <61211556+ExcplitBugle@users.noreply.github.com>
2021-03-22 03:18:12 -0400

    Update constants.py
    Added Token!

--- Bot/constants.py
+++ Bot/constants.py
@@ -1,1 +1,10 @@
-# Junk so i can upload it to github
+from os import getenv
+from discord import Colour
+
+from dotenv import load_dotenv
+
+class Logins:
+    load_dotenv()
+
+    BOT_TOKEN = getenv("BOT_TOKEN")
+    #Adding DB INFO Later!
