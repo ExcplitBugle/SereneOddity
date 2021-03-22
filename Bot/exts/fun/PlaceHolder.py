48600cafc9865514780b1fc702d4e6bd21dafed3
Braiden <61211556+ExcplitBugle@users.noreply.github.com>
2021-03-22 04:43:06 -0400

    fun.py
    Added DrunkIfy Text!

--- Bot/exts/fun/PlaceHolder.py
+++ Bot/exts/fun/PlaceHolder.py
@@ -1,1 +1,21 @@
-# Junk so i can upload it to github
+import discord, asyncio, random, time, datetime
+from discord.ext import commands
+from discord.ext.commands import clean_content
+
+class TextConverters(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+  
+  
+    @commands.command(aliases=['mock'])
+    async def drunkify(self, ctx, *, s):
+        lst = [str.upper, str.lower]
+        newText = await commands.clean_content().convert(ctx, ''.join(random.choice(lst)(c) for c in s))
+        if len(newText) <= 380:
+            await ctx.send(newText)
+        else:
+            try:
+                await ctx.author.send(newText)
+                await ctx.send(f"**{ctx.author.mention} The output too was too large, so I sent it to your DMs! :mailbox_with_mail:**")
+            except Exception:
+                await ctx.send(f"**{ctx.author.mention} There was a problem, and I could not send the output. It may be too large or malformed**")
